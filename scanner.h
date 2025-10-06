b79a6adcdc346cb9b5f35db1507d44869f459123
tihlaw <williamtihelka@gmail.com>
2025-10-06 08:39:52 +0200

    Refactor token type definitions

--- scanner.h
+++ scanner.h
@@ -2,40 +2,52 @@
 // scanner.h by William Denis "xtihelw00" Tihelka on MM/DD/25.
 //
 typedef enum {
-    TOKEN_EOF,             // end of file
-    TOKEN_EOL,             // end of line
+    TT_EOF,             // end of file
+    TT_EOL,             // end of line
 
-    TOKEN_IDENTIFIER,      // identifier
-    TOKEN_KEYWORD,         // keyword(while,if,...)
-    TOKEN_NULL,            // null literal
+    TT_IDENTIFIER,      // identifier
+    TT_NULL,            // null literal
+    TT_INT,             // intiger
+    TT_FLOAT,           // float
+    TT_STRING,          // string
 
-    TOKEN_INT_LITERAL,     // intiger
-    TOKEN_FLOAT_LITERAL,   // float
-    TOKEN_STRING_LITERAL,  // string
+    // Keywords defined by language
+    TT_KEYWORD_CLASS,   // class
+    TT_KEYWORD_IF,      // if
+    TT_KEYWORD_ELSE,    // else
+    TT_KEYWORD_IS,      // is
+    TT_KEYWORD_RETURN,  // return
+    TT_KEYWORD_VAR,     // var
+    TT_KEYWORD_WHILE,   // while
+    TT_KEYWORD_IFJ,     // Ifj
+    TT_KEYWORD_STATIC,  // static
+    TT_KEYWORD_IMPORT,  // import
+    TT_KEYWORD_FOR,     // for
+    TT_KEYWORD_NUM,     // num
+    TT_KEYWORD_Null,    // Null
 
-    // Operátory
-    TOKEN_ASSIGN,          // =
-    TOKEN_OPERATOR_PLUS,   // +
-    TOKEN_OPERATOR_MINUS,  // -
-    TOKEN_OPERATOR_MUL,    // *
-    TOKEN_OPERATOR_DIV,    // /
-    TOKEN_OPERATOR_EQ,     // ==
-    TOKEN_OPERATOR_NEQ,    // !=
-    TOKEN_OPERATOR_LT,     // <
-    TOKEN_OPERATOR_GT,     // >
-    TOKEN_OPERATOR_LE,     // <=
-    TOKEN_OPERATOR_GE,     // >=
-    TOKEN_OPERATOR_IS,     // is
+    // Operators
+    TT_ASSIGN,          // =
+    TT_PLUS,            // +
+    TT_MINUS,           // -
+    TT_MUL,             // *
+    TT_DIV,             // /
+    TT_EQ,              // ==
+    TT_NEQ,             // !=
+    TT_LT,              // <
+    TT_GT,              // >
+    TT_LE,              // <=
+    TT_GE,              // >=
 
-    // Oddělovače a symboly
-    TOKEN_LPAREN,          // (
-    TOKEN_RPAREN,          // )
-    TOKEN_LBRACE,          // {
-    TOKEN_RBRACE,          // }
-    TOKEN_COMMA,           // ,
-    TOKEN_DOT,             // .
+    // Symbols
+    TT_LPAREN,          // (
+    TT_RPAREN,          // )
+    TT_LBRACE,          // {
+    TT_RBRACE,          // }
+    TT_COMMA,           // ,
+    TT_DOT,             // .
 
-    TOKEN_ERROR            // error (unknown char) - might be replaces with just error message
+    TT_ERROR            // error (unknown char) - might be replaces with just error message
 } TokenType;
 
 typedef struct {
