016587b4ccc893edcf7776b64f38d0a0638bde50
xlevic02 <xlevic02@stud.fit.vut.cz>
2025-10-11 18:49:37 +0200

    added error.c and error.h

---
+++ src/error.c
@@ -0,0 +1,12 @@
+// Implementace prekladace imperativniho jazyka IFJ25
+// Error by Jan Frantisek "xlevic02" Levicek on 10/3/24.
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+#include "error.h"
+
+void error(int err_num, const char* err_message) {
+    fprintf(stderr,err_message);
+    exit(err_num);
+}
